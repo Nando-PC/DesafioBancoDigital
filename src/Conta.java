41ea5bdba6452120c484afc9c12e476f1d3e68c5
Nando-PC <nandowarrior@gmail.com>
2022-10-14 17:46:16 -0300

    Primeiro commit desafio Banco Digital - Java e Orientação a Objetos

---
+++ src/Conta.java
@@ -0,0 +1,43 @@
+public abstract class Conta implements iConta{
+
+    //atributos a serem usados no Construtor
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    //Construtor da Conta
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+
+    }
+}
