8de48e6c5568e6c93ad06a8a3b10b28005bb0aec
Nando-PC <nandowarrior@gmail.com>
2022-10-19 17:13:07 -0300

    Desafio Banco Digital com JAVA/Orientação a Objetos

--- src/Conta.java
+++ src/Conta.java
@@ -7,11 +7,13 @@ public abstract class Conta implements iConta{
     protected int agencia;
     protected int numero;
     protected double saldo;
+    protected Cliente cliente;
 
     //Construtor da Conta
-    public Conta() {
+    public Conta(Cliente cliente) {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
     }
 
     public int getAgencia() {
@@ -28,16 +30,26 @@ public abstract class Conta implements iConta{
 
     @Override
     public void sacar(double valor) {
+        this.saldo -= valor;
 
     }
 
     @Override
     public void depositar(double valor) {
+        this.saldo += valor;
 
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
 
+    protected void informacoesComuns(){
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
 }
